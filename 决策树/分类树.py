51a1b8bcababd341fd51f0235c555981a89e057b
hongghu <hongghu.@outlook.com>
2019-10-25 14:45:34 +0800

    分类树

--- 决策树/分类树.py
+++ 决策树/分类树.py
@@ -42,10 +42,10 @@ print(clf.apply(Xtest))
 feature_name = ['酒精','苹果酸','灰','灰的碱性','镁','总酚','类黄酮','非黄烷类酚类'
     ,'花青素','颜色强度','色调','od280/od315稀释葡萄酒','脯氨酸']
 
-with open('tee.dot', 'w') as dot_tree:
+with open('tee.gv', 'w') as dot_tree:
     dot_tree=tree.export_graphviz(clf,
                                   feature_names=feature_name,
-                                  class_names=['胡红光','胡希奥','话还是'],
+                                  class_names=['c1','c2','c3'],
                                   filled=True,
                                   rounded=True,
                                   out_file=dot_tree,
@@ -61,3 +61,35 @@ with open('tee.dot', 'w') as dot_tree:
 fi=[*zip(feature_name,clf.feature_importances_)]
 # for i in fi:
 #     print(i)
+
+
+dot_tree = tree.export_graphviz(clf,
+                                feature_names=feature_name,
+                                class_names=['c1', 'c2', 'c3'],
+                                filled=True,
+                                rounded=True,
+                                special_characters=True
+
+                                )
+import sys
+import os
+os.environ["PATH"] += os.pathsep + r'D:\Application\graphviz\bin'
+
+import pydotplus
+graph = pydotplus.graph_from_dot_data(dot_tree)
+#生成pdf
+graph.write_pdf("wine.pdf")
+#生成png
+graph.write_png("wine.png")
+
+
+#这三行代码可以生成pdf：
+# import graphviz
+# dot_data = tree.export_graphviz(dot_tree, out_file=None)
+# graph = graphviz.Source(dot_data)
+# graph.render()
+
+## 使用ipython的终端jupyter notebook显示。
+# from IPython.display import Image
+# graph = pydotplus.graph_from_dot_data(dot_tree)
+# Image(graph.create_png(), width = 800, height = 800)
