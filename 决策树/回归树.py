76dc0345d078853f35bfd4c70dd222df014e334f
hongghu <hongghu.@outlook.com>
2019-10-25 11:10:05 +0800

    回归树

--- 决策树/回归树.py
+++ 决策树/回归树.py
@@ -12,7 +12,8 @@ from sklearn.tree import DecisionTreeRegressor
 
 boston = load_boston()
 regressor = DecisionTreeRegressor(random_state=321)
+
+# 交叉验证cross_val_score的用法
 cross_val_score(regressor, boston.data, boston.target
                 , cv=10
                 , scoring='neg_mean_squared_error')
-# 交叉验证cross_val_score的用法
