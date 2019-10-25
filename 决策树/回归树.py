c4af43842b8e08cefcf100024220edb0c4b63ba7
hongghu <hongghu.@outlook.com>
2019-10-25 11:02:51 +0800

    回归树

---
+++ 决策树/回归树.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# coding:utf-8
+
+"""
+@software: PyCharm
+@file: 回归树.py
+"""
+
+from sklearn.datasets import load_boston
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeRegressor
+
+boston = load_boston()
+regressor = DecisionTreeRegressor(random_state=321)
+cross_val_score(regressor, boston.data, boston.target
+                , cv=10
+                , scoring='neg_mean_squared_error')
+# 交叉验证cross_val_score的用法
